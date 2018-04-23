2c47dc4b9ca02d23fe2ee136680f02b4c7d38f68
Sharrod Hines <sharrodhines@gmail.com>
2018-04-23 01:42:24 -0400

    Initial commit

---
+++ src/components/AddQuote.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="row">
+      <form>
+        <div class="colsm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+            <label>Quote</label>
+            <textarea rows="3" class="form-control" v-model="quote"></textarea>
+        </div>
+        <div class="colsm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+            <button class="btn btn-primary" @click.prevent="addQuote">Add Quote</button>
+        </div>
+      </form>
+  </div>
+</template>
+
+<script>
+  import Quote from './Quote.vue'
+
+export default {
+    data: function() {
+        return {
+            quote: ''
+        };
+    },
+    methods: {
+        addQuote() {
+            this.$emit('quoteAdded', this.quote);
+            this.quote = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button {
+        display: block;
+        margin: 0 auto;
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12">
+            <h3>Quotes Added</h3>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
+                    :style="{width: (quoteCount / maxQuotes) * 100 + '%'}">
+                    {{quoteCount}} / {{maxQuotes}}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['quoteCount', 'maxQuotes'],
+    }
+</script>
+
+<style scoped>
+    #progressBar {
+        width: 100%;
+        height: 30px;
+        border-radius: 5px;
+        background-color: lightgrey;
+        margin-bottom: 40px;
+    }
+
+    #progress {
+        background-color: #4c4cff;
+        height: 30px;
+        border-radius: 5px;
+        box-shadow: 1px 1px 2px #ccc;
+        text-align: center;
+        color: #FFF;
+    }
+</style>
+
